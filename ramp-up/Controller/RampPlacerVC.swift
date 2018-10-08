3d9b320659932ae5c6545d18e657d56d0d10cde8
Richard Cummings <richardecummings@gmail.com>
2018-10-07 23:17:50 -0400

    init

--- ramp-up/Controller/RampPlacerVC.swift
+++ ramp-up/Controller/RampPlacerVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RampPlacerVC.swift
 //  ramp-up
 //
 //  Created by Richard Cummings on 2018-10-07.
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class RampPlacerVC: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
